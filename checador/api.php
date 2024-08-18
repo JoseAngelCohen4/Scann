49a5fe32f2d7e56be9b481c148ce0835f40d4a6f
JoseAngelCohen4 <114305789+JoseAngelCohen4@users.noreply.github.com>
2024-08-17 22:34:04 -0700

    Add files via upload
    parte final del checador

--- checador/api.php
+++ checador/api.php
@@ -1,6 +1,7 @@
 <?php
 header('Content-Type:application/json');
-if(!empty($_GET['id_empleado'])){
+if(!empty($_GET['id_empleado']))
+{
     $query="SELECT * FROM empleados WHERE id_empleado = $_GET[id_empleado]";
     $Conecion=mysqli_connect("localhost","root","","nomina");
     $resultado=mysqli_query($Conecion,$query);
@@ -12,9 +13,32 @@ if(!empty($_GET['id_empleado'])){
         $data["nombre"] = "{$fila['nombre_empleado']} {$fila['ap1_empleado']} {$fila['ap2_empleado']}";
         $data["correo"] =$fila['correo_empleado'];
         $data["foto"] ="http://localhost/fotos/".$fila["foto_empleado"];
-        print_r($data);
+        response(200,$data);
     }
     else{
+        $data['id_empleado']="";
+        $data["nombre"] = "";
+        $data["correo"] ="";
+        $data["foto"] ="";
+        response(300,$data);
+    }   
+}
+else{
+        $data['id_empleado']="";
+        $data["nombre"] = "";
+        $data["correo"] ="";
+        $data["foto"] ="";
+        response(400,$data);
+    }
+
+    function response ($status,$data){
+        header("HTTP/1.1 ".$status);
+        $response["status"]=$status;
+        $response["id_empleado"]=$data["id_empleado"];
+        $response["id_nombre"]=$data["nombre"];
+        $response["id_correo"]=$data["correo"];
+        $response["id_foto"]=$data["foto"];
+        $json_encode=json_encode($response);
+        echo $json_encode;
 
     }
-}
